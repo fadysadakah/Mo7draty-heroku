d8a4a8d828b8dce38c3a72dc5de630bd40957a9f
fadysadakah.emh@gmail.com <fadysadakah.emh@gmail.com>
2019-04-12 18:19:27 +0200

    signup

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
+var aaa;
 var express = require('express');
 var app = express();
 var request = require('request');
-
-// request('http://www.google.com', function (error, response, body) {
-//     console.log('error:', error); // Print the error if one occurred
-//     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//     console.log('body:', body); // Print the HTML for the Google homepage.
-// });
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
-// req.body.parameter
 app.use(express.static('public'));
 const PORT = process.env.PORT || 5000;
 app.set('view engine', 'ejs');
@@ -17,9 +11,14 @@ var cookieParser = require('cookie-parser')
 app.use(cookieParser());
 const pg = require('pg');
 var md5 = require('md5');
-var image='';
+var request_first_name;
+var request_last_name;
+var request_email;
+var request_password;
+var request_confirm_password;
+var s;
+var script = "" ;
 // #########################################################################################################################################################################################
-
 const client = new pg.Client({
     user: 'vknjruhfiarkkd',
     password: 'b3cf54047b0ad8234bd442e966993c1eb2a84e47fef50972e8c601de34ed9e83',
@@ -31,50 +30,41 @@ const client = new pg.Client({
 
 client.connect();
 
-
-
-
 // #########################################################################################################################################################################################
-
 app.get('/', function (req, res) {
-
-    client.query("SELECT * FROM users WHERE email = 'fadysadakah.emh@gmail.com'", function(err,RES)  {
-        if (err) console.log(err.detail);
-        if (RES.rows.length != 0) {
-
-            first_name = RES.rows[0].first_name;
-            last_name = RES.rows[0].last_name;
-            var password = RES.rows[0].password;
-             image = RES.rows[0].image;
-
-            }
-            res.render("index", {image: image });
-            
-  
-    })
-
- 
+    res.render('index')
 });
-
 // #########################################################################################################################################################################################
-
-
 app.get('/sign_up', function (req, res) {
-    res.render('sign_up')
+    res.render('sign_up', { max: '', script: script})
 })
-
-
-
 // #########################################################################################################################################################################################
 app.post('/sign_up', function (req, res) {
-    
-    res.redirect('/')
+    request_first_name = req.body.first_name;
+    request_last_name = req.body.last_name;
+    request_email = req.body.email;
+    request_password = req.body.password;
+    request_confirm_password = req.body.confirm_password;
+    if (request_password == request_confirm_password) {
+        if (request_password.length < 8) {
+            res.render('sign_up', { max: "too short password" })
+        } else {
+            client.query("SELECT * FROM users WHERE email  = '" + request_email + "'", function (err, result) {
+                if (err) {
+                    res.render('sign_up', { max: "error" });
+                } else if (result.rowCount == 0) {
+                    client.query("INSERT INTO users (first_name,last_name,email,password) VALUES ('" + request_first_name + "','" + request_last_name + "','" + request_email + "','" + request_password + "')", function (err, result) {
+                    })
+                    res.render('sign_up', { max: "account added" });
+                } else {
+                    res.render('sign_up', { max: "user exists" });
+                }
+            })
+        }
+    } else {
+        res.render('sign_up', { max: "passwords mismatch" });
+    }
 })
-
-
-
-
-
 
 
 
@@ -88,3 +78,6 @@ app.get('*', function (req, res) {
 app.listen(PORT, function () {
     console.log('Server Started')
 })
+
+
+// ################################################################################################################################################################################################
